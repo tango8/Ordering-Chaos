c2187746ffc523ec667c5df91a5ce15df0e58ccc
tango8 <ethan.b.tang@gmail.com>
2025-10-04 14:28:55 -0400

    Made things work, yes

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,18 +2,18 @@ extends Node2D
 
 func _ready():
 	# Load Stage
-	var stage_scene = load("res://scenes/Stage.tscn") as PackedScene
+	var stage_scene = load("res://scenes/stage.tscn") as PackedScene
 	var stage_instance = stage_scene.instantiate()
 	add_child(stage_instance)
 
 	# Load Player
-	var player_scene = load("res://scenes/Player.tscn") as PackedScene
+	var player_scene = load("res://scenes/player.tscn") as PackedScene
 	var player_instance = player_scene.instantiate()
 	add_child(player_instance)
 
 	# Camera follows player
 	var camera = Camera2D.new()
-	camera.current = true
 	player_instance.add_child(camera)
+	camera.make_current()
 
 	print("Main scene ready: Stage + Player loaded")
