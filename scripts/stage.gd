c9f04da7761d0d3a1e7a1261a082260e871f16cb
naushadwaqar <nwaqar970209@gmail.com>
2025-10-04 15:16:47 -0400

    idk

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -1,9 +1,9 @@
 extends StaticBody2D
 
 func _ready():
-	# Assign a rectangle shape at runtime to avoid parser errors
-	if $CollisionShape2D.shape == null:
-		var rect = RectangleShape2D.new()
-		rect.size = Vector2(1000,50)
-		$CollisionShape2D.shape = rect
-	print("Stage loaded!")
+    # Assign a rectangle shape at runtime to avoid parser errors
+    if $CollisionShape2D.shape == null:
+        var rect = RectangleShape2D.new()
+        rect.size = Vector2(1000,50)
+        $CollisionShape2D.shape = rect
+    print("Stage loaded!")
