20086e0d343da1f9c541bd23dd578f9717a0068a
tango8 <ethan.b.tang@gmail.com>
2025-10-04 14:59:12 -0400

    gui scene stuff done, we doin gui scene stuff now

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,19 +1,10 @@
 extends Node2D
 
 func _ready():
-	# Load Stage
-	var stage_scene = load("res://scenes/stage.tscn") as PackedScene
-	var stage_instance = stage_scene.instantiate()
-	add_child(stage_instance)
+	var player = $Player
+	var stage = $Stage
 
-	# Load Player
-	var player_scene = load("res://scenes/player.tscn") as PackedScene
-	var player_instance = player_scene.instantiate()
-	add_child(player_instance)
+	# Example: move player to starting position
+	player.position = Vector2(-200, 150)
 
-	# Camera follows player
-	var camera = Camera2D.new()
-	player_instance.add_child(camera)
-	camera.make_current()
-
-	print("Main scene ready: Stage + Player loaded")
+	print("Main scene ready")
